b0fdc8b8c5576b23ac93099f4226323c53434476
Courtney Rowlands <54596326+crowlands1010@users.noreply.github.com>
2019-08-30 15:24:35 -0500

    Update fileDownload.R

--- fileDownload.R
+++ fileDownload.R
@@ -2,6 +2,6 @@ if (!file.exists("data")) {
   dir.create("data")
 }
 
-fileUrl <- "https://data.baltimorecity.gov/Transportation/Fixed-Speed-Cameras/aqgr-xx9h/data" ##testdata
-download.file(fileUrl,destfile="./data/cameras.csv", method = "libcurl") ##teststorage
+fileUrl <- "https://thiswillbetheurltosomesortofdatafile" ##testdata
+download.file(fileUrl,destfile="./a/place/to/store/data", method = "libcurl") ##teststorage
 list.files("./data")
