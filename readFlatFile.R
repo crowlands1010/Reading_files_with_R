bc6f6f23f70bda2d4c6998d3a228273427dc4032
Courtney Rowlands <54596326+crowlands1010@users.noreply.github.com>
2019-08-30 15:52:59 -0500

    Update readFlatFile.R

--- readFlatFile.R
+++ readFlatFile.R
@@ -2,5 +2,6 @@
 ##important parameters: file, header, separator, row.names, and nrows
 ##this file is specific to a lecture on coursera, but can be modified.
 
-cameraData <- read.table("./data/cameras.csv", sep=",",header=TRUE)
+##read.csv can be interchanged with read.table. I'm reading a .csv in this specific instance.
+cameraData <- read.csv("./data/cameras.csv", sep=",",header=TRUE)
 head(cameraData)
